73b98dff5d9922f9da5b0b897e52a91f846f6c9c
JoeNeu <johannes3008@hotmail.com>
2021-11-05 16:13:21 +0100

    update home styling

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -10,7 +10,11 @@ export class HomeComponent implements OnInit {
   public randomVideo = [
     "https://vid.pr0gramm.com/2019/07/20/e228bff6488cedd8.mp4",
     "https://vid.pr0gramm.com/2020/10/01/3fc1daa84470c188.mp4",
-    "https://vid.pr0gramm.com/2019/07/21/4f8f029532c5add1.mp4"
+    "https://vid.pr0gramm.com/2019/07/21/4f8f029532c5add1.mp4",
+    "https://vid.pr0gramm.com/2017/03/31/fecff413c5bfbf4e.mp4",
+    "https://vid.pr0gramm.com/2017/04/01/ade13d1bf713480e.mp4",
+    "https://vid.pr0gramm.com/2018/06/18/5783dbab386e6586.mp4",
+    "https://vid.pr0gramm.com/2018/06/23/48ec3c0750819100.mp4"
   ]
   public displayVideo;
 
