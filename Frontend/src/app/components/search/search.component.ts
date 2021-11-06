8f4b8143458d265abeb0603e9da8edfbe35256ab
RenaMa <rena.roxie@gmail.com>
2021-11-06 09:46:29 +0100

    add movie search input

--- Frontend/src/app/components/search/search.component.ts
+++ Frontend/src/app/components/search/search.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  movieSearchValue = 'Title';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
